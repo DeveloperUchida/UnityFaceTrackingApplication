ec59e40028391a2132dc258cdd28934055f8a3fb
DeveloperUchida <kouei28poiou@icloud.com>
2024-02-01 15:24:10 +0900

    DataUpdate 2024/02/01

--- Assets/Scripts/FoldingSystemScripts.cs
+++ Assets/Scripts/FoldingSystemScripts.cs
@@ -1,10 +1,11 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 public class FoldingSystemScripts : MonoBehaviour
 {
     private GameObject FoldinfImage;
 
-    private bool openFold = false;
+    private bool openFold;
 
     private RectTransform button;
 
@@ -13,14 +14,46 @@ public class FoldingSystemScripts : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        FoldinfImage = transform.Find("Image2").gameObject;
+        FoldinfImage = transform.Find("UnityButtonimage").gameObject;
         button = transform.Find("image").GetComponent<RectTransform>();
         contentLayout = transform.Find("Content").GetComponent<VerticalLayoutGroup>();
     }
 
     // Update is called once per frame
-    void Update()
+    /* void Update()  //使用していない関数はコメント化しています
     {
+         
+     }
+     */
+
+    public void OnclicksetInfo()
+    {
+        if (openFold)
+        {
+            button.localRotation = Quaternion.Euler(0, 0, -90);
+            FoldinfImage.SetActive(false);
+
+            StartCoroutine("LayoutRenewal");
+
+            openFold = true;
+        }
+        else
+        {
+            button.localRotation = Quaternion.Euler(0,0,90);
+            FoldinfImage.SetActive(true);
+
+            StartCoroutine("LayoutRenewal");
+        }
+
+        contentLayout.enabled = !contentLayout.enabled;
+    }
+
+    IEnumerator LayoutRenewal()
+    {
+        //1フレーム停止
+        yield return null;
         
+        //再スタート後の処理
+        contentLayout.enabled = !contentLayout.enabled;
     }
 }
